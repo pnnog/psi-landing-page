2f962a822bb4aa38e6125975eb32a86c3797a21c
pnnog <pnnogdev@gmail.com>
2023-08-01 22:56:18 -0300

    feat: added favicon

--- src/components/SectionImage/index.jsx
+++
@@ -1,7 +0,0 @@
-const SectionImage = ({src, alt}) =>{
-    return (
-        <img id="section-image" src={src} alt={alt} className=" max-h-[600px] md:shadow-md hover:scale-105 transition-transform duration-700 lg:justify-self-end rounded-full" />
-    )
-}
-
-export default SectionImage
